eb37a05df5b05dd2c3be03b73dd400b5109b71af
Haonan Chang <chnme40cs@gmail.com>
2024-04-01 17:52:06 -0400

    update: update the download link

--- tools/download_data.py
+++ tools/download_data.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
     download_key = "kowndi_pusht_demo_repulse_no_control"
     url_dict = {
-        "kowndi_pusht_demo_repulse_no_control": "https://drive.google.com/file/d/1cVdygalbELc7DZYGJB1cu7bbYQ2tueRv/view?usp=drive_link",
+        "kowndi_pusht_demo_repulse_no_control": "https://drive.usercontent.google.com/u/1/uc?id=1cVdygalbELc7DZYGJB1cu7bbYQ2tueRv&export=download",
     }
     url = url_dict[download_key]
     output = f"{data_dir}/{download_key}.zarr.tar.xz"
