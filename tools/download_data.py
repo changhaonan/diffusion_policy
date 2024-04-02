0b082bc27ef7ba35673802539c853f6a2900b890
Haonan Chang <chnme40cs@gmail.com>
2024-04-02 19:06:40 -0400

    updae: update download file

--- tools/download_data.py
+++ tools/download_data.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
     data_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
-    download_key = "kowndi_pusht_demo_repulse_no_control"
+    download_key = "kowndi_pusht_demo_v0_repulse"
     url_dict = {
         "kowndi_pusht_demo_repulse_no_control": "https://drive.google.com/file/d/1cVdygalbELc7DZYGJB1cu7bbYQ2tueRv/view?usp=sharing",
         "kowndi_pusht_demo_v0_repulse": "https://drive.google.com/file/d/1XUG6f9qKxwG8lDJ3r64Y36X4L8Lpz-HJ/view?usp=sharing",
