efa267d555595a25d2bba0d5233c07546a713981
Haonan Chang <chnme40cs@gmail.com>
2024-04-07 14:45:43 -0400

    update: remove tempary change

--- tools/data_augmentation.py
+++ tools/data_augmentation.py
@@ -1,10 +1,11 @@
-import os 
+import os
 import random
 import numpy as np
 import zarr
 import cv2
 from tqdm.auto import tqdm
 from tools.dataset_manipulate import read_from_path, convert
+
 
 def get_bbox(points=None, original_bbox=None, num_augmentations=2, min_margin=0.05, max_margin=0.1):
     """Get bounding box from the trajectories."""
@@ -19,7 +20,7 @@ def get_bbox(points=None, original_bbox=None, num_augmentations=2, min_margin=0.
     # Calculate initial width and height
     initial_width = max_x - min_x
     initial_height = max_y - min_y
-    
+
     bounding_boxes = []
     margin_per = [min_margin, max_margin]
     for _ in range(num_augmentations):
@@ -36,12 +37,13 @@ def get_bbox(points=None, original_bbox=None, num_augmentations=2, min_margin=0.
             top_left = (top_left[0], 0)
         if bottom_right[0] > 96:
             bottom_right = (96, bottom_right[1])
-        if bottom_right[1] > 96:   
+        if bottom_right[1] > 96:
             bottom_right = (bottom_right[0], 96)
         bounding_box = {"top_left": top_left, "bottom_right": bottom_right}
         bounding_boxes.append(bounding_box)
-    
+
     return bounding_boxes
+
 
 def save_dict2zarr(data, group):
     for key, value in data.items():
@@ -51,6 +53,7 @@ def save_dict2zarr(data, group):
         else:
             group.create_dataset(key, data=np.array(value))
 
+
 if __name__ == "__main__":
     root_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
     src_data = "kowndi_pusht_demo_v0_region.zarr"
@@ -59,43 +62,42 @@ if __name__ == "__main__":
 
     render_size = 96
     root = read_from_path(os.path.join(root_dir, src_data))
-    
+
     aug_root = {
-        "data" : {
-            "state" : [],
-            "control" : [],
-            "img" : [],
-            "action" : [],
-            "n_contacts" : [],
+        "data": {
+            "state": [],
+            "control": [],
+            "img": [],
+            "action": [],
+            "n_contacts": [],
         },
-        "meta" : {
-            "episode_ends" : [],
-        }
+        "meta": {
+            "episode_ends": [],
+        },
     }
-    
+
     for i in tqdm(range(0, len(root["meta"]["episode_ends"])), desc="Copying initial data"):
         aug_root["meta"]["episode_ends"].append(root["meta"]["episode_ends"][i])
         if i == 0:
             traj_indices = range(root["meta"]["episode_ends"][i])
         else:
-            traj_indices = range(root["meta"]["episode_ends"][i-1], root["meta"]["episode_ends"][i])
+            traj_indices = range(root["meta"]["episode_ends"][i - 1], root["meta"]["episode_ends"][i])
         for j in tqdm(traj_indices, desc="Loading data", leave=False):
             aug_root["data"]["state"].append(root["data"]["state"][j])
             aug_root["data"]["img"].append(root["data"]["img"][j])
             aug_root["data"]["action"].append(root["data"]["action"][j])
             aug_root["data"]["n_contacts"].append(root["data"]["n_contacts"][j])
             aug_root["data"]["control"].append(root["data"]["control"][j])
-    
 
     for i in tqdm(range(0, len(root["meta"]["episode_ends"]), 2), desc="Augmenting New Data"):
         original_episode_ends = aug_root["meta"]["episode_ends"][-1]
         if i == 0:
-            traj_indices = range(root["meta"]["episode_ends"][i]) 
+            traj_indices = range(root["meta"]["episode_ends"][i])
         else:
-            traj_indices = range(root["meta"]["episode_ends"][i-1], root["meta"]["episode_ends"][i])
+            traj_indices = range(root["meta"]["episode_ends"][i - 1], root["meta"]["episode_ends"][i])
         aug_root["meta"]["episode_ends"].append(len(traj_indices) + original_episode_ends)
 
-        control_img = root["data"]["control"][traj_indices[0]] 
+        control_img = root["data"]["control"][traj_indices[0]]
 
         # Detect the green channel being significantly higher than the other two in the green area
         green_mask = (control_img[:, :, 1] > control_img[:, :, 0]) & (control_img[:, :, 1] > control_img[:, :, 2])
@@ -105,7 +107,7 @@ if __name__ == "__main__":
         assert coordinates.shape[0] > 0
         top_left = [coordinates[:, 0].min(), coordinates[:, 1].min()]
         bottom_right = [coordinates[:, 0].max(), coordinates[:, 1].max()]
-        
+
         bboxes = get_bbox(points=None, original_bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
         bbox = bboxes[0]
         controls = np.array([bbox["top_left"], bbox["bottom_right"]])
