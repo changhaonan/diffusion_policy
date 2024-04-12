e69cc896b2795d00a3ebd90035b7a7316e589ac4
Haonan Chang <chnme40cs@gmail.com>
2024-04-11 20:28:50 -0400

    update: update manipulate tool

--- tools/dataset_manipulate.py
+++ tools/dataset_manipulate.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import zarr
+import random
 from tqdm.auto import tqdm
 
 
@@ -16,6 +17,8 @@ def read_from_path(zarr_path, mode="r"):
 def convert(root, export_path, convertion_str, **kwargs):
     if convertion_str == "control2no_control":
         return control2no_control(root, export_path, **kwargs)
+    elif convertion_str == "control2ratio_control":
+        return control2ratio_control(root, export_path, **kwargs)
     else:
         raise ValueError(f"Unknown convertion_str: {convertion_str}")
 
@@ -51,12 +54,64 @@ def control2no_control(root, export_path):
     return export_root
 
 
-if __name__ == "__main__":
-    root_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
-    src_data = "kowndi_pusht_demo_repulse.zarr"
-    tar_data = "kowndi_pusht_demo_repulse_no_control.zarr"
-    tar_data = os.path.join(root_dir, tar_data)
-    convertion_str = "control2no_control"
+def control2ratio_control(root, export_path, ratio=0.5):
+    """Convert control data to ratio% control data + no control data."""
+    data = root["data"]
+    meta = root["meta"]
+    episode_ends = np.array(meta["episode_ends"])
+    data_rc = {}
+    episode_ends_rc = [0]
+    for key, value in data.items():
+        data_rc[key] = []
 
-    root = read_from_path(os.path.join(root_dir, src_data))
-    convert_root = convert(root, tar_data, convertion_str)
+    # Create export zarr file
+    export_root = zarr.open(export_path, "w")
+    export_data = export_root.create_group("data")
+    export_meta = export_root.create_group("meta")
+
+    # Traverse the data
+    count_nc = 0
+    count_c = 0
+    for i in tqdm(range(len(episode_ends) - 1)):
+        start, end = episode_ends[i], episode_ends[i + 1]
+        control = data["control"][start:end]
+        if np.sum(control) == 0:
+            # No control data
+            for key, value in data.items():
+                data_rc[key].append(value[start:end])
+            episode_ends_rc.append(episode_ends_rc[-1] + end - start)
+            count_nc += 1
+        else:
+            if random.random() < ratio:
+                # Control data
+                for key, value in data.items():
+                    data_rc[key].append(value[start:end])
+                episode_ends_rc.append(episode_ends_rc[-1] + end - start)
+                count_c += 1
+
+    print(f"count_nc: {count_nc}, count_c: {count_c}")
+    # Save the data
+    for key, value in data_rc.items():
+        export_data.create_dataset(key, data=np.concatenate(value, axis=0))
+    export_meta.create_dataset("episode_ends", data=episode_ends_rc)
+    return export_root
+
+
+if __name__ == "__main__":
+    server_type = "ilab"
+    netid = "hc856"
+    control_type = "repulse"
+    if server_type == "local":
+        data_src = "./data"
+    elif server_type == "ilab":
+        data_src = f"/common/users/{netid}/Project/diffusion_policy/data"
+
+    ratio = 0.7
+    src_data = f"kowndi_pusht_demo_v0_{control_type}.zarr"
+    tar_data = f"kowndi_pusht_demo_v0_{control_type}_rc_{ratio}.zarr"
+    tar_data = os.path.join(data_src, tar_data)
+    # convertion_str = "control2no_control"
+    convertion_str = "control2ratio_control"
+
+    root = read_from_path(os.path.join(data_src, src_data))
+    convert_root = convert(root, tar_data, convertion_str, ratio=ratio)
