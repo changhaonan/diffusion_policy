dc9f4755f0c5afe6deb5eac9dbf398536ffa00fb
Haonan Chang <chnme40cs@gmail.com>
2024-04-13 16:31:09 -0400

    update: polished the tools

--- tools/check_dataset.py
+++ tools/check_dataset.py
@@ -14,36 +14,43 @@ def read_from_path(zarr_path, mode="r"):
     return root
 
 
-def visualize_dataset(root, wait_time=18):
+def visualize_dataset(root, wait_time=18, shuffle=False):
     data = root["data"]
     meta = root["meta"]
     episode_ends = np.array(meta["episode_ends"])
     print(f"Number of episodes: {len(episode_ends)}")
     for i in range(0, len(episode_ends), 1):
+        if shuffle:
+            i = np.random.randint(0, len(episode_ends))
         if i == 0:
-            start = 0
+            continue
         else:
             start = episode_ends[i - 1]
         end = episode_ends[i]
         print(f"Episode {i}: {start} -> {end}")
         image = data["img"][start:end]
         control = data["control"][start:end]
+        demo_type = data["demo_type"][start:end].max()
         for j in range(len(image)):
             vis_image = cv2.addWeighted(image[j], 0.5, control[j], 0.5, 0)
+            # Add text on demo_type
+            cv2.putText(vis_image, f"D: {demo_type}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
             cv2.imshow(f"episode-{i}", vis_image)
             cv2.waitKey(wait_time)
         cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    server_type = "ilab"
+    wait_time = 100
+    server_type = "ilab" if os.path.exists("/common/users") else "local"
     netid = "hc856"
+    data_version = 1
     control_type = "repulse"
     if server_type == "local":
         data_src = "./data"
     elif server_type == "ilab":
         data_src = f"/common/users/{netid}/Project/diffusion_policy/data"
 
-    src_data = f"{data_src}/kowndi_pusht_demo_v0_{control_type}.zarr"
+    src_data = f"{data_src}/kowndi_pusht_demo_v{data_version}_{control_type}.zarr"
     root = read_from_path(src_data)
-    visualize_dataset(root)
+    visualize_dataset(root, wait_time=wait_time, shuffle=True)
--- tools/download_tool.py
+++ tools/download_tool.py
@@ -5,10 +5,10 @@ import os
 def download_dataset(data_src, download_key: str):
     print(f"Start download {download_key}...")
     url_dict = {
-        "kowndi_pusht_demo_repulse_no_control": "https://drive.google.com/file/d/1cVdygalbELc7DZYGJB1cu7bbYQ2tueRv/view?usp=sharing",
-        "kowndi_pusht_demo_v0_repulse": "https://drive.google.com/file/d/1XUG6f9qKxwG8lDJ3r64Y36X4L8Lpz-HJ/view?usp=sharing",
-        "kowndi_pusht_demo_v0_follow": "https://drive.google.com/file/d/1AohGsWMvZuaYsFM4slEEL5MO7LC9zQO0/view?usp=sharing",
-        "kowndi_pusht_demo_v0_region": "https://drive.google.com/file/d/1DkKaj-t5GF2inKlpJQ32pT7_UpxtKBSr/view?usp=sharing",
+        f"kowndi_pusht_demo_v0_repulse": "https://drive.google.com/file/d/1XUG6f9qKxwG8lDJ3r64Y36X4L8Lpz-HJ/view?usp=sharing",
+        f"kowndi_pusht_demo_v0_follow": "https://drive.google.com/file/d/1AohGsWMvZuaYsFM4slEEL5MO7LC9zQO0/view?usp=sharing",
+        f"kowndi_pusht_demo_v0_region": "https://drive.google.com/file/d/1DkKaj-t5GF2inKlpJQ32pT7_UpxtKBSr/view?usp=sharing",
+        f"kowndi_pusht_demo_v1_repulse": "https://drive.google.com/file/d/1LOywDZMYe6N_rmeBVSE0Ywm2TjdQ9rQU/view?usp=sharing",
     }
     url = url_dict[download_key]
     output = f"{data_src}/{download_key}.zarr.tar.xz"
@@ -26,14 +26,15 @@ def download_dataset(data_src, download_key: str):
 if __name__ == "__main__":
     import os
 
+    data_version = 1
     server_type = "local" if not os.path.exists("/common/users") else "ilab"
     netid = "hc856"
-    control_type = "region"
+    control_type = "repulse"
     if server_type == "local":
         data_src = "./data"
     elif server_type == "ilab":
         data_src = f"/common/users/{netid}/Project/diffusion_policy/data"
 
     ## Download the dataset
-    download_key = f"kowndi_pusht_demo_v0_{control_type}"
+    download_key = f"kowndi_pusht_demo_v{data_version}_{control_type}"
     download_dataset(data_src, download_key)
